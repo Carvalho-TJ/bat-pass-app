35186697f8ec2dead215fbccbf72291fc4518519
Tiago R Carvalho <tiagorc.dev@gmail.com>
2024-01-12 16:56:06 -0400

    criando o input e demais alteraçoes

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import styles from '../Home/Style';
+import { BatLogo } from '../../components/BatLogo/BatLogo';
+import { BatTextInput } from '../../components/BatTextInput/BatTextInput';
+
+
+export default function Home() {
+  return (
+    <View style={styles.appContainer}>
+      
+      <View style={styles.logoContainer}>
+        <BatLogo/>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <BatTextInput/>
+      </View>
+
+      <StatusBar style="light" />
+    </View>
+  );
+}
